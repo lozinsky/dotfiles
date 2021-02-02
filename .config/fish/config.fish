939718dc5ff458b102d58762ede491fbde82510d
Pavel Lozinsky <pavel@lozinsky.dev>
2021-02-03 01:30:38 +0300

    feat(fish): add go binaries to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
-set -gx PATH "/opt/homebrew/bin" $PATH
-
 set -gx DOTFILES "$HOME/.dotfiles"
 set -gx GOPATH "$HOME/.go"
+
+set -gx PATH "/opt/homebrew/bin" $PATH
+set -gx PATH "$GOPATH/bin" $PATH
 
 alias serve "servor --reload"
 alias touch-bar-reset "killall ControlStrip"
