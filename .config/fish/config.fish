f438d90ed4207127a39c861553716cf78d5066a3
Pavel Lozinsky <pavel@lozinsky.dev>
2022-12-26 13:43:57 +0400

    feat: add `rust` installation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ set -gx DOTFILES "$HOME/.dotfiles"
 set -gx GOPATH "$HOME/.go"
 
 set -gx PATH "/opt/homebrew/bin" $PATH
+set -gx PATH "$HOME/.cargo/bin" $PATH
 set -gx PATH "$GOPATH/bin" $PATH
 
 alias serve "servor --reload"
